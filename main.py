42dbda114fc0d8246364d4668e6c587faf80931c
ngynmhieu <ngyn.mhieu@gmail.com>
2024-03-29 12:16:44 +0700

    update main and simple ai

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from simple_ai import *
 
 AIO_FEED_IDs = ["nutnhan1", "nutnhan2"]
 AIO_USERNAME = "ngmhieu"
-AIO_KEY = "aio_ldIT28hezujV6Nba2zkRnIN0qhp5"
+AIO_KEY = ""
 
 def connected(client):
     print("Ket noi thanh cong ...")
